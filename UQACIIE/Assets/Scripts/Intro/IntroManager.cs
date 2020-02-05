54bc0369c85adc73a14a85f7778709e5c25e17a9
Elias NACEF <elias.nacef@ensiie.fr>
2020-02-05 14:17:11 +0100

    Config du systeme de particules eventuel et du son de la scene d'intro

--- UQACIIE/Assets/Scripts/Intro/IntroManager.cs
+++ UQACIIE/Assets/Scripts/Intro/IntroManager.cs
@@ -12,7 +12,14 @@ public class IntroManager : MonoBehaviour
 
     IEnumerator WaifForIntroAnimation()
     {
-        yield return new WaitForSeconds(5f);
+        AudioManager audioManager = FindObjectOfType<AudioManager>();
+        yield return new WaitForSeconds(1.5f);
+        audioManager.Play("Intro1");
+        yield return new WaitForSeconds(0.8f);
+        audioManager.Play("Intro2");
+        yield return new WaitForSeconds(0.2f);
+        audioManager.Play("Intro3");
+        yield return new WaitForSeconds(2f);
         SceneManager.LoadScene("MenuScene");
     }
 }
